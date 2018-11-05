6b0ab23f7d06753a95dda27ec8f7a75326061d85
hurrikam <hurrikam@hotmail.it>
2018-11-05 00:48:18 +0000

    Tag filtering

--- src/htmlScript.js
+++ src/htmlScript.js
@@ -1,24 +1,59 @@
 'use strict';
 
-function showFeature(activeIndex) {
+function getSelectedTags() {
+    const tags = [];
+    document.querySelectorAll('.tag-list input[type="checkbox"]')
+        .forEach(checkbox => {
+            if (checkbox.checked) {
+                tags.push(checkbox.value);
+            }
+        });
+    return tags;
+}
+
+function filterScenariosInFeature(feature) {
+    const tags = getSelectedTags();
+    return feature.scenarios
+        .filter(scenario => {
+            const scenarioTags = scenario.tags;
+            const isScenarioWithoutTags = tags.includes('') && scenarioTags.length === 0;
+            const scenarioMatchesSomeTags = tags.some(tag => scenarioTags.includes(tag));
+            return isScenarioWithoutTags || scenarioMatchesSomeTags;
+        });
+}
+
+function updateFeatureList() {
     document.querySelectorAll('.feature-list button')
         .forEach((button, index) => {
-            const buttonClassLit = button.classList;
-            if (index === activeIndex) {
-                buttonClassLit.add('active');
+            const feature = features[index];
+            const filteredScenariosCount = filterScenariosInFeature(feature).length;
+            if (filteredScenariosCount > 0) {
+                button.classList.remove('button-no-scenarios');
             } else {
-                buttonClassLit.remove('active');
+                button.classList.add('button-no-scenarios');
             }
+            button.querySelector('.button-scenario-count').textContent =
+                `${filteredScenariosCount}`;
         });
-    document.querySelectorAll('.feature-details .feature')
-        .forEach((featureDiv, index) => {
-            const featureClassLit = featureDiv.classList;
-            if (index === activeIndex) {
-                featureClassLit.remove('feature-hidden');
-            } else {
-                featureClassLit.add('feature-hidden');
-            }
-        });
+}
+
+function showFeature(index) {
+    const feature = features[index];
+    const filteredScenarios = filterScenariosInFeature(feature);
+    const featureDetails = document.querySelector('.feature-details');
+    featureDetails.innerHTML = `
+        <h3>${feature.name}</h3>    
+        <ol>
+        ${filteredScenarios
+            .map((scenario, index) =>
+                `<li class="scenario-name
+                    ${index % 2 ? 'scenario-name-light' : 'scenario-name-dark'}
+                    ${scenario.tags.join(' ')}    
+                ">${scenario.name}
+                </li>`)
+            .join('')
+        }
+        </ol>`;    
 }
 
 window.onload = () => document.querySelector('.feature-list button').click();
